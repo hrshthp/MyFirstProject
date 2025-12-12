b7e1352d13b305bab8d23bb58560907c35cfa910
Harshitha <harshithavijaydeep@gmail.com>
2025-12-12 18:02:44 +0530

    final

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -16,7 +16,6 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
   return (
     <form action={formAction}>
       {" "}
-      /*form action=createInvoice*/
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
